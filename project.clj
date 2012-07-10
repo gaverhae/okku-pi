f44a60b00958df4b6234db480162f8da250d24d5
Gary Verhaegen <gary.verhaegen@gmail.com>
2012-07-10 19:26:23 +0200

    Added Okku dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject okku-pi "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Okku Pi tutorial"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure.gaverhae/okku "0.1.0-SNAPSHOT"]])
