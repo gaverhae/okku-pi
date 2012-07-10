3d6ef6437b3435bff757ba5e4da10743f0cd7d4c
Gary Verhaegen <gary.verhaegen@gmail.com>
2012-07-10 19:10:03 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to okku-pi
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/okku_pi/core_test.clj
@@ -0,0 +1,7 @@
+(ns okku-pi.core-test
+  (:use clojure.test
+        okku-pi.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
