f5128c22e9e8e42ef72a38a1821a8c19876f4c7a
Gary Verhaegen <gary.verhaegen@gmail.com>
2012-07-10 19:30:47 +0200

    Added instruction to run the program

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "Okku Pi tutorial"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main okku-pi.core
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure.gaverhae/okku "0.1.0-SNAPSHOT"]])
