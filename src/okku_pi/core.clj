6d1bb9d6ee77eb261439583ee5d3e569b8f53495
Gary Verhaegen <gary.verhaegen@gmail.com>
2012-07-10 19:44:59 +0200

    Added the message definitions

--- src/okku_pi/core.clj
+++ src/okku_pi/core.clj
@@ -1,6 +1,15 @@
 (ns okku-pi.core
   (use okku.core))
 
+(defn m-compute []
+  {:type :compute})
+(defn m-work [start n-elem]
+  {:type :work :start start :n-elem n-elem})
+(defn m-result [value]
+  {:type :result :value value})
+(defn m-approx [pi dur]
+  {:type :approx :pi pi :dur dur})
+
 (defn -main
   "I don't do a whole lot."
   [& args]
