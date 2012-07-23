94e6c3dc1ea529e3285c1c088b4c9f6e5edf1033
Gary Verhaegen <gary.verhaegen@gmail.com>
2012-07-23 16:39:44 +0200

    Removed doc and test directories

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject okku-pi "0.1.1"
+(defproject okku-pi "0.1.2-SNAPSHOT"
   :description "Okku Pi tutorial"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main okku-pi.core
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure.gaverhae/okku "0.1.1"]])
+                 [org.clojure.gaverhae/okku "0.1.2-SNAPSHOT"]])
